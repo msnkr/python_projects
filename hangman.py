7f19862bda2669ba8314f5683a9d62faf865c407
msnkr <74895279+msnkr@users.noreply.github.com>
2022-01-11 08:41:30 +0200

    .

---
+++ hangman.py
@@ -0,0 +1,41 @@
+import random
+
+name = input('What is your name?: ')
+print(f'Goodluck {name}! ')
+
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+
+
+word = random.choice(words)
+guesses = ''
+turns = 12
+
+while turns > 0:
+    failed = 0
+
+    for char in word:
+        if char in guesses:
+            print(char)
+
+        else:
+            print('_')
+            failed += 1
+
+    if failed == 0:
+        print('You Win! ')
+        print(f'The word is: {word}.')
+        break
+
+    guess = input('Guess a character: ')
+    guesses += guess
+
+    if guess not in word:
+        turns -= 1
+
+        print('Wrong')
+        print(f'You have {turns} guesses left! ')
+
+        if turns == 0:
+            print('You lose! ')
