999466e2edc851de6785364e45dd3e0ffbea76e4
msnkr <74895279+msnkr@users.noreply.github.com>
2022-01-13 14:15:37 +0200

    Create bagels.py

---
+++ bagels.py
@@ -0,0 +1,12 @@
+import random
+
+num_digits = 3
+max_guesses = 10
+
+
+def main():
+    print('I am thinking of a {}- digit number with no repeated digits. Try to guess the clues: When I say "Pico", That means One digit is correct but in the wrong position. When I say "Fermi", That means One digit is correct and in the right position. When I say "Bagels", No digit is correct. For example ')
+
+
+if __name__ == '__main__':
+    main()
