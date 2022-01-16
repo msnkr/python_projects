5a91066d98b14ab7c6da35ddbc179f75df7d9d02
msnkr <mikyle.designs@gmail.com>
2022-01-16 21:44:25 +0200

    .

---
+++ your_life_in_weeks.py
@@ -0,0 +1,7 @@
+age = input('What is your age?: ')
+max_age = 90
+
+age = int(age)
+max_age -= age
+
+print(f'You have {max_age * 365} days left, {max_age * 52} weeks left and {max_age * 12} months left.')
