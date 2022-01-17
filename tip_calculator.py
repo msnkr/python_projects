808d98d8702e5c7a97868d0bd559d364429eb7d6
msnkr <74895279+msnkr@users.noreply.github.com>
2022-01-17 10:45:58 +0200

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,13 +1,11 @@
 # TOTAL BILL
-bill = float(input('What is the total amount?: '))
-# TOTAL BILL X 10, 12, 15 
-tip = input('How much tip would you like to leave?: ')
+bill = float(input("What is the amount on the bill?: $"))
+# TOTAL BILL X 10, 12, 15
+tip = int(input('How much tip would you like to leave?: 10, 12 or 15% '))
 # BILL / 100, REMAINDER BILL X 100
-percentage = bill / 100 
-tip = percentage * 100
+percentage = (tip / 100) * bill + bill
 # SPLIT THE BILL WITH 2 DECIMAL PLACES
-people = int(input('How many people?'))
+split = int(input('How many people should the bill be split by?: '))
+split2 = percentage / split
 
-split = round(tip, 2) 
-
-print(split / people)
+print(f'Every person should pay ${round(split2, 2)}')
