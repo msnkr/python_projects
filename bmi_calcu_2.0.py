d6722b33ee128aaa40f50627cfe6ef938c966ca4
msnkr <74895279+msnkr@users.noreply.github.com>
2022-01-18 11:20:54 +0200

    Update bmi_calcu_2.0.py

--- bmi_calcu_2.0.py
+++ bmi_calcu_2.0.py
@@ -1,17 +1,16 @@
-# height = float(input('What is your height?: '))
-# weight = float(input('What is your weight?: '))
+height = float(input('What is your height?: '))
+weight = float(input('What is your weight?: '))
 
-# bmi = float(weight / (height ** 2))
+bmi = float(weight / (height ** 2))
 
-bmi = int(input('bmi '))
 
 if bmi < 18.5:
     print(f'Your BMI is {round(bmi)}. You are slightly underweight. ')
-elif bmi < 18.5 <= 25:
+elif bmi < 25:
     print(f'Your BMI is {round(bmi)}. You have normal weight.')
-elif bmi > 25 <= 30:
+elif bmi < 30:
     print(f'Your BMI is {round(bmi)}. You are slightly overweight.')
-elif bmi > 30 < 35:
+elif bmi < 35:
     print(f'Your BMI is {round(bmi)}. You are obese.')
 else:
     print(f'Your BMI is {round(bmi)}. You are clinically obese')
