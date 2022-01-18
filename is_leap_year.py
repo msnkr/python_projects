fa5932ba40f17237395d396f86f6094dd8fbe914
msnkr <mikyle.designs@gmail.com>
2022-01-18 23:23:53 +0200

    .

--- is_leap_year.py
+++ is_leap_year.py
@@ -1,7 +1,11 @@
 year = int(input('Which year do you want to check?: '))
 
-four = year / 4
-hundred = year / 100
-hundred4 = year / 400
-if year == int(year):
-    print(year)
+one = year / 4
+two = year / 100
+three = year / 400
+
+
+if one == int(one) or two == int(two) or three == int(three):
+            print('Leap')
+else:
+    print('Not a leap year.')
