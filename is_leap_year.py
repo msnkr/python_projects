afd2494027de42318df436555c05413eb97e134d
msnkr <74895279+msnkr@users.noreply.github.com>
2022-01-19 11:48:54 +0200

    Update is_leap_year.py

--- is_leap_year.py
+++ is_leap_year.py
@@ -1,11 +1,20 @@
 year = int(input('Which year do you want to check?: '))
 
-one = year / 4
-two = year / 100
-three = year / 400
+# one = year / 4
+# two = year / 100
+# three = year / 400
 
 
-if one == int(one) or two == int(two) or three == int(three):
-            print('Leap')
+# if one == int(one) or two == int(two) or three == int(three):
+#     print('Leap Year')
+# else:
+#     print('Not a leap year.')
+if year % 4 == 0:
+    if year % 100 != 0:
+        print('Leap Year')
+    elif year % 400 == 0:
+        print('Leap Year')
+    else:
+        print('Not a leap year')
 else:
-    print('Not a leap year.')
+    print('Not leap year.')
