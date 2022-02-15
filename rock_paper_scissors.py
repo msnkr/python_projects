bf448718c4645b26a3348959580132677efb41e3
msnkr <mikyle.designs@gmail.com>
2022-02-15 16:05:46 +0200

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -29,13 +29,27 @@ scissors = '''
 import random
 
 
-answer = input('What do you chose? 0 for Rock, 1 for Paper, 2 for Scissors.')
-com_choice = [rock, paper, scissors]
+answer = input('What do you chose? 0 for Rock, 1 for Paper, 2 for Scissors. ')
 
+if answer == 0:
+    answer = rock
+elif answer == 1:
+    answer = paper
+elif answer == 2:
+    answer = scissors
+
+com_choice = [rock, paper, scissors]
 random_choice = random.choice(com_choice)
 
-if answer == com_choice:
-    print(f'You chose \n {answer}')
-    print(f'The computer chose \n {com_choice}')
 
-if answer == rock
+print(answer)
+print()
+print()
+print()
+print()
+print(random_choice)
+
+# if answer == random_choice:
+#     print(f'You chose \n {answer}! ')
+#     print(f'computer chose \n {random_choice}! ')
+#     print('Draw!')
