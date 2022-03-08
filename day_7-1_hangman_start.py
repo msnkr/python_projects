6a6bb86252033f96e0c5e7591ab8fc8903877d4e
msnkr <mikyle.designs@gmail.com>
2022-03-08 22:47:20 +0200

    .

--- day_7-1_hangman_start.py
+++ day_7-1_hangman_start.py
@@ -3,15 +3,16 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-word = random.choice(word_list)
+chosen_word = random.choice(word_list)
 
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-letter = input('Choose a letter to start. \n')
+guess = input('Guess a letter:  ').lower()
 
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if letter in  word:
-    print('Letter is in word')
-else:
-    print('Try again')
+for item in chosen_word:
+    if guess == item:
+        print(f'{guess}')
+    else:
+        print('_')
