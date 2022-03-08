a73bcbb3a7fd74047a111ea412da7e664056de3f
msnkr <mikyle.designs@gmail.com>
2022-03-08 16:14:29 +0200

    .

--- test.py
+++ test.py
@@ -100,8 +100,11 @@
 
 # hello_world('Hello', 'Mikyle')
 
-x = 0
+# x = 0
 
-while x < 10:
-    print(x)
-    x+=1
+# while x < 10:
+#     print(x)
+#     x+=1
+#     if x == 5:
+#         print('Your at 5')
+# print('Done')
