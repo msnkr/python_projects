55fa11b1c682a1b957f8c9d1173a63060506152e
msnkr <mikyle.designs@gmail.com>
2022-03-17 12:20:36 +0200

    Update day_8-6_caesar_cipher_start.py

--- day_8-6_caesar_cipher_start.py
+++ day_8-6_caesar_cipher_start.py
@@ -4,26 +4,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
-    encode_text = ''
+def caesar(direction, text, shift):
+    cipher_text = ''
+    for letter in text:
+        item = alphabet.index(letter)
+        if direction == 'encode':
+            cipher_text += alphabet[item + shift]
+        elif direction == 'decode':
+            cipher_text += alphabet[item - shift]
+    print(cipher_text)
+        
 
-    for i in text:
-        num = alphabet.index(i)
-        item = alphabet[num + shift]
-        encode_text += item
-    print(f'the encoded text is {encode_text}. ')
-
-def decrypt(text, shift):
-    decode_text = ''
-
-    for i in text:
-        num = alphabet.index(i)
-        item = alphabet[num + -shift]
-        decode_text += item
-    print(f'the decoded text is {decode_text}. ')
-
-
-if direction == 'encode':
-    encrypt(text, shift)
-elif direction == 'decode':
-    decrypt(text, shift)
+caesar(direction, text, shift)
