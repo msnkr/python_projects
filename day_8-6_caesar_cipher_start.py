9f1b4165164289b32483e860721afd77f54f9d4b
msnkr <mikyle.designs@gmail.com>
2022-03-17 20:23:52 +0200

    .

--- day_8-6_caesar_cipher_start.py
+++ day_8-6_caesar_cipher_start.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'r', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -12,7 +12,7 @@ def caesar(direction, text, shift):
             cipher_text += alphabet[item + shift]
         elif direction == 'decode':
             cipher_text += alphabet[item - shift]
-    print(cipher_text)
+    print(f'the {direction}d text is {cipher_text}. ')
         
 
 caesar(direction, text, shift)
