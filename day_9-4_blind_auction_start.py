01f1310eb699efed5819304651cfa1761f2c13eb
msnkr <mikyle.designs@gmail.com>
2022-03-28 22:20:58 +0200

    .

--- day_9-4_blind_auction_start.py
+++ day_9-4_blind_auction_start.py
@@ -20,19 +20,15 @@ bid_again = True
 while bid_again:
     name = input('What is your name? ')
     bid = int(input('What is your bid? $'))
-    
     current_bid = {
         'name': name,
         'bid': bid
     }
-
     bidders.append(current_bid)
+
     play_again = input('Would you like to bid again? Y/N \n')
     if play_again == 'n':
         bid_again = False
-
         
-    os.system('clear')
-
-       
-print(bidders[0])
+        print(bidders[0]['bid'])
+                
