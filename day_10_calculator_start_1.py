30232baab1b432f947a134b7dc8b3b396e091eb8
msnkr <mikyle.designs@gmail.com>
2022-04-13 21:42:02 +0200

    .

--- day_10_calculator_start_1.py
+++ day_10_calculator_start_1.py
@@ -21,15 +21,6 @@ operations = {
     '/': divide
 }
 
-num1 = int(input('What is the first number?: '))
 
-for operation in operations:
-    print(operation)
-symbol = input('Which operator would you like to use?: ')
-num2 = int(input('What is the second number?: '))
 
-operation_symbol = operations[symbol]
-answer = operation_symbol(num1, num2)
-
-print(f'{num1} {symbol} {num2} = {answer}')
 
