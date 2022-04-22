774b53c6a67b2fb4e3348ec4eea228262850f136
msnkr <mikyle.designs@gmail.com>
2022-04-22 13:01:29 +0200

    Update day_10_calculator_start_1.py

--- day_10_calculator_start_1.py
+++ day_10_calculator_start_1.py
@@ -21,25 +21,8 @@ operations = {
     '/': divide
 }
 
-should_continue = True
-while should_continue:
-    num1 = int(input('What is the First number?: '))
-    for operator in operations:
-        print(operator)
-    operators = input('Choose a operator: ')
-    next_num = int(input('What is the second number?: '))
 
-    operator = operations[operators]
-    answer = operator(num1, next_num)
-    print(f'{num1} {operators} {next_num} = {answer}')
 
-    again = input('Would you like to go again? ')
-    if again == 'n':
-        print('Goodbye!')
-        should_continue = False
-    next_num = int(input('What is the second number?: '))
-    operators = input('Choose a operator: ')
-    new_answer = operator(answer, next_num)
 
-    
-    print(f'{answer} {operators} {next_num} = {new_answer}')
+
+
