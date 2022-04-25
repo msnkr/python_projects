d5c51b1ecf50b01453004ff2a7d8d039d15ae26b
msnkr <mikyle.designs@gmail.com>
2022-04-25 13:57:34 +0200

    Update day_10_calculator_start_1.py

--- day_10_calculator_start_1.py
+++ day_10_calculator_start_1.py
@@ -21,6 +21,16 @@ operations = {
     '/': divide
 }
 
+def first_answer():
+    num1 = int(input('Enter the first number: '))
+    for item in operations:
+        print(item)
+    operator = input('Select a operator: ')
+    num2 = int(input('Enter the second number: '))
+
+    operator_symbol = operations[operator]
+    answer = operator_symbol(num1, num2)
+    second_answer(answer)
 
 
 
