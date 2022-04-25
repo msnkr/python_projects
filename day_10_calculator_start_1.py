c45a28aa37d6912eafadb312ed6f5fad1ab5ecfd
msnkr <mikyle.designs@gmail.com>
2022-04-25 22:06:24 +0200

    .

--- day_10_calculator_start_1.py
+++ day_10_calculator_start_1.py
@@ -21,16 +21,52 @@ operations = {
     '/': divide
 }
 
-def first_answer():
-    num1 = int(input('Enter the first number: '))
-    for item in operations:
-        print(item)
-    operator = input('Select a operator: ')
-    num2 = int(input('Enter the second number: '))
+calculator_art = """
+ _____________________
+|  _________________  |
+| | JO           0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+"""
 
-    operator_symbol = operations[operator]
-    answer = operator_symbol(num1, num2)
-    second_answer(answer)
+def calculator():
+    print(calculator_art)
+    should_continue = True
+    num1 = float(input('Enter the first number: '))
+
+
+    while should_continue:
+        for item in operations:
+            print(item)
+        operator = input('Select a operator: ')
+        num2 = float(input('Enter the Next number: '))
+
+        operator_symbol = operations[operator]
+        answer = operator_symbol(num1, num2)
+        print(f'{num1} {operator} {num2} = {answer}')
+        go_again = input(f'Do you want to go again? with {answer}? Y. Start again? N. e to Exit \n').lower()
+
+        if go_again == 'y':
+            num1 = answer
+        elif go_again == 'n':
+            should_continue = False
+            calculator()
+        else:
+            should_continue = False
+            print('Goodbye!')
+
+
+calculator()
+
 
 
 
