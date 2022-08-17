1cdebafa62e48939ebe420e118f25bc86ca02571
msnkr <mikyle.designs@gmail.com>
2022-08-17 11:07:44 +0200

    Update day_12_-Guess_the_number.py

--- day_12_-Guess_the_number.py
+++ day_12_-Guess_the_number.py
@@ -1,39 +1,39 @@
-import random
-import os
+# import random
+# import os
 
-def play_again():
-    tries = 0
-    random_number = random.randint(1, 101)
-    print(random_number)
+# def play_again():
+#     tries = 0
+#     random_number = random.randint(1, 101)
+#     print(random_number)
 
 
-    easy_hard = input('Easy or Hard Mode?: ').lower()
-    if easy_hard == 'easy' or 'e':
-        tries = 10
-    else:
-        tries = 5
+#     easy_hard = input('Easy or Hard Mode?: ').lower()
+#     if easy_hard == 'easy' or 'e':
+#         tries = 10
+#     else:
+#         tries = 5
 
-    flag = True
-    while flag:
-        pick_num = int(input('Enter Your guess: '))
-        if tries == 0:
-            print('You Lose. No more tries left. ')
-            flag = False
-        elif pick_num < random_number:
-            print('Your number was too low. Try again: ')
-            tries -= 1
-        elif pick_num > random_number:
-            print('Your number was too high. Try again: ')
-            tries -= 1
-        else:
-            print('You got the correct number. Congrats.')
-            flag = False
+#     flag = True
+#     while flag:
+#         pick_num = int(input('Enter Your guess: '))
+#         if tries == 0:
+#             print('You Lose. No more tries left. ')
+#             flag = False
+#         elif pick_num < random_number:
+#             print('Your number was too low. Try again: ')
+#             tries -= 1
+#         elif pick_num > random_number:
+#             print('Your number was too high. Try again: ')
+#             tries -= 1
+#         else:
+#             print('You got the correct number. Congrats.')
+#             flag = False
 
-    if input('Do you want to go again?: ') == 'y':
-        os.system('clear')
-        play_again()
+#     if input('Do you want to go again?: ') == 'y':
+#         os.system('clear')
+#         play_again()
 
-play_again()
+# play_again()
 
 ###################3 Her KAK VERSION ########################
 
@@ -90,4 +90,7 @@ play_again()
 
 
 # game()
+
+
+
 
