62a92fcfeb96ae965a803aa2271b8ef21e558692
msnkr <mikyle.designs@gmail.com>
2022-08-18 13:49:20 +0200

    .

--- new_test.py
+++ new_test.py
@@ -249,7 +249,7 @@
 # for key in programming_dictionary:
 #     print(f'{key}: {programming_dictionary[key]}')
 
-    #This selects each key and value from dictionary
+#     This selects each key and value from dictionary
 
 
 # Nesting 
@@ -509,12 +509,12 @@
 
 # print(alphabet)
 
-################################################################################################################################################################
+# ###############################################################################################################################################################
 
 # alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 # for item in alphabet:
 #     print(item, end=' ')
-    #End removes the escape character and replaces it with ' ' or /. You can put anything to replace the escape character
+#     End removes the escape character and replaces it with ' ' or /. You can put anything to replace the escape character
 
 # seperator replaces the escape character too 
 # print('19', '12', '1990', sep='/')
@@ -559,7 +559,7 @@
 # print(list)
 
 
-#Swapping variables 
+# Swapping variables 
 # x, y = 20, 30
 # x, y = y, x
 # print(x, y)
@@ -577,44 +577,44 @@
 # print(f'The name appears {count} times.')
 
 
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+# 11 is the Ace.
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
+# Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+# user_cards = []
+# computer_cards = []
 
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
+# Hint 6: Create a function called calculate_score() that takes a List of cards as input 
+# and returns the score. 
+# Look up the sum() function to help you do this.
 
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+# Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+# Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
 
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+# Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
 
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+# Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
 
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
+# Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
 
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+# Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
 
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+# Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo
+# Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo
 
 
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
+# Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+# 11 is the Ace.
 
 # import random
 # import os
 
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+# Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+# Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 # def compare(user_score, computer_score):
 #     if user_score == computer_score:
 #         return 'It\'s a draw'
@@ -697,7 +697,7 @@
 
 
 
-####################################scope#######################################
+# ###################################scope#######################################
 
 # enemies = 1
 
@@ -709,4 +709,14 @@
 # print(f'How many enemies now?: {enemies}')
 
 
-#####################
+# ####################
+
+
+turns = 10
+
+def new_turns():
+    return turns - 1
+
+
+turns = new_turns()
+print(turns)
