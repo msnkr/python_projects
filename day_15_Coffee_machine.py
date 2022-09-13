5acca287de95a36c0b89f3be32eafbc2a123ce0e
msnkr <mikyle.designs@gmail.com>
2022-09-13 22:39:17 +0200

    .

--- day_15_Coffee_machine.py
+++ day_15_Coffee_machine.py
@@ -34,20 +34,23 @@ def pay_now(cost):
     quaters = int(input('How many qauters?: '))
     for item in range(quaters + 1):
         quaters = item * 0.25
-
     dimes = int(input('How many dimes?: '))
     for item in range(dimes + 1):
         dimes = item * 0.10
-
     nickels = int(input('How many nickels?: '))
     for item in range(nickels + 1):
-        nickels = item * 0.5
-
+        nickels = item * 0.05
     pennies = int(input('How many pennies?: '))
     for item in range(pennies + 1):
-        pennies = item * 0.1
+        pennies = item * 0.01
 
-    return f"quaters{quaters} dimes{dimes} + nickels{nickels} + pennies{pennies}"
+    total = quaters + dimes + nickels + pennies
+    if cost > total:
+        return f'There is not enough money. Returned {round(total, 2)}c'
+    elif cost < total:
+        return f'Here is your change {round(total - cost, 2)}'
+    else:
+        return 'No change.'
 
 
 def is_enough(my_drink):
@@ -58,16 +61,24 @@ def is_enough(my_drink):
         drink_milk = drink_resources['milk']
         drink_water = drink_resources['water']
         drink_coffee = drink_resources['coffee']
-    
+
     if drink_coffee < resources['coffee'] and drink_milk < resources['milk'] and drink_water < resources['water']:
         return my_drink['cost']
+    else:
+        return False
 
 
-my_drink = input('What do you want to drink?: Espresso/Latte/Capaccino ')
 
-if my_drink == 'report':
-    for _ in resources:
-        print(f'The current resources are: {_}: {resources[_]}')
-
-cost = is_enough(my_drink)
-print(pay_now(cost))
+not_off = False
+while not_off != True:
+    my_drink = input('What do you want to drink?: Espresso/Latte/Cappuccino ')
+    if my_drink == 'off':
+        not_off = True
+    elif my_drink == 'report':
+        for _ in resources:
+            print(f'The current resources are: {_}: {resources[_]}')
+    else:
+        if is_enough(my_drink) != False:
+            cost = is_enough(my_drink)
+            print(pay_now(cost))
+            
