8620e4789ff6d3d2720dcb8e6785ac81056a5251
msnkr <mikyle.designs@gmail.com>
2022-10-07 22:27:33 +0200

    .

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,8 +1,16 @@
 from question_model import Question
 from data import question_data
 
+question_bank = []
+for item in question_data:
+    question_text = item['text']
+    question_answer = item['answer']
 
-#Iterate over each question
-#create new question object 
-#Append to new list
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+    
 
+print(question_bank[2].text)
+# Iterate over each question
+# create new question object 
+# Append to new list
