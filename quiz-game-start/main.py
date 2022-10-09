6e69948ebb03b62599054913386eab596fd7d95f
msnkr <mikyle.designs@gmail.com>
2022-10-09 22:46:12 +0200

    .

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -4,9 +4,13 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for item in question_data:
-    new_question = Question(item['text'], item['answer'])
+    new_question = Question(item['question'], item['correct_answer'])
     question_bank.append(new_question)
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print('The quiz is done.')
+print(f'Your total score is: {quiz.score}/{quiz.question_number} ')
