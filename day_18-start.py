633bb012a6eccdd4903ab80e2ad340c9f9010370
msnkr <mikyle.designs@gmail.com>
2022-10-12 22:34:59 +0200

    .

--- day_18-start.py
+++ day_18-start.py
@@ -4,7 +4,7 @@ timmy = Turtle()
 timmy.color('MediumVioletRed')
 
 
-# timmy.speed(5)
+timmy.speed(5)
 # for _ in range(4):
 #     timmy.dot(20, 'SlateBlue')
 #     timmy.forward(200)
@@ -26,9 +26,40 @@ timmy.color('MediumVioletRed')
 import random
 
 
-colors = ['red', 'green', 'purple', 'yellow', 'black', 'SlateBlue', 'MediumVioletRed', 'orange', 'salmon']
+# colors = ['red', 'green', 'purple', 'yellow', 'black', 'SlateBlue', 'MediumVioletRed', 'orange', 'salmon']
+# timmy.pendown()
+
+# Random walk
+# def draw(num_sides):
+#     timmy.color(random.choice(colors))
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         timmy.forward(100)
+#         timmy.right(angle)
+
+# for sides in range(3, 11):
+#     draw(sides)
+
+
 timmy.pendown()
+timmy.hideturtle()
+timmy.pensize(10)
+random_heading = [0, 90, 180, 270]
+colors = ['SlateBlue', 'MediumVioletRed', 'DeepPink', 'OrangeRed', 'Gold', 'Tan', 'Khaki', 'Chartreuse', 'Cyan', 'DodgerBlue', 'Blue', 'DimGray', 'Gainsboro']
+
+def draw(side):
+    for _ in range(side):
+        timmy.color(random.choice(colors))
+        timmy.forward(25)
+        timmy.setheading(random.choice(random_heading))
+        
+
+    
+for side in range(100):
+    draw(side)
+
 
 
 screen = Screen()
+screen.screensize(canvwidth=300, canvheight=300)
 screen.exitonclick()
