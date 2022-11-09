8ae055156551f56bc6063a4853a26d3ebbc60cbe
msnkr <mikyle.designs@gmail.com>
2022-11-09 10:32:58 +0200

    .

--- random_num.py
+++ random_num.py
@@ -1,36 +1,47 @@
 import random
-from turtle import Screen
+import PySimpleGUI as sg
 
 
-screen = Screen()
+sg.theme('DarkAmber')
+
 area_code = '+27'
 operator_num = ['84', '78', '76', '71', '73', '74', '64', '62', '81', '61', '82', '72', '79', '63', '60', '83']
 random_numbers = []
 
-try:
-    how_many = screen.textinput(title='NumberGen', prompt='How Many Numbers?: ')
-    how_many = int(how_many)
-    name_file = screen.textinput(title='NumberGen', prompt='What to name the file?: ')
-    # work with net
 
-    def get_numbers():
-        new_num = ''
-        for _ in range(7):
-            new_num += str(random.randint(0, 9))
-            number = area_code + random.choice(operator_num) + new_num
-        random_numbers.append(number)
+def get_numbers():
+    new_num = ''
+    for _ in range(7):
+        new_num += str(random.randint(0, 9))
+        number = area_code + random.choice(operator_num) + new_num
+    random_numbers.append(number)
 
 
-    def save_numbers():
-        with open(f'{name_file}.txt', 'w') as f:
-            for num in random_numbers:
-                f.write(f'{num}\n')
+def save_numbers():
+    with open(f'{name_file}.txt', 'w') as f:
+        for num in random_numbers:
+            f.write(f'{num}\n')
 
 
-    for _ in range(how_many - 1):
-        get_numbers()
+how_many = 'How Many Numbers?: '
+name_file = 'What to name the file?: '
 
-    save_numbers()
-            
-except TypeError():
-    quit()
+layout = [
+    [sg.Text(how_many), sg.InputText()],
+    [sg.Text(name_file), sg.InputText()],
+    [sg.Button('Create'), sg.Button('Cancel')],
+]
+
+window = sg.Window('Bleggh', layout)
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    else:
+        new_value = int(values[1])
+        for _ in range(new_value - 1):
+            get_numbers()
+
+
+save_numbers()
+window.close()
