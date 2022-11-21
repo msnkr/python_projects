e7d13db0254ac244b61721deac7c027c8657081e
msnkr <mikyle.designs@gmail.com>
2022-11-21 22:35:50 +0200

    .

--- day_22_main.py
+++ day_22_main.py
@@ -11,17 +11,22 @@ screen.tracer(0)
 
 timmy = Player()
 car = CarManager()
+car_positions = []
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
     car.create_car()
     car.move_cars()
+
     screen.listen()
     screen.onkeypress(timmy.move_up, 'Up')
 
-    
+    for x in car.cars_list:
+        first_car = x[0]
+        if first_car.xcor() == 0:
+            break
+
 
 screen.exitonclick()
