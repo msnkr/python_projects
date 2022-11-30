8f76976e1e81adc9a2ec9fa5ffc08f604cc78c20
msnkr <mikyle.designs@gmail.com>
2022-11-30 21:56:32 +0200

    .

--- day_22_main.py
+++ day_22_main.py
@@ -12,3 +12,10 @@ screen.tracer(0)
 time.sleep(0.1)
 
 screen.exitonclick()
+
+# Create a turtle object
+# Move it forward
+# Create multiple cars and move them to the left
+# If car crashes the turtle then break
+# If turtle gets to the top then restart position and increase speed of cars
+# Create a score board with current level of cars and if game over
