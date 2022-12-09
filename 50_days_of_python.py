3650cc862d5afa15a5c494df33141e7dcdf3e58d
msnkr <mikyle.designs@gmail.com>
2022-12-09 09:53:56 +0200

    Create 50_days_of_python.py

---
+++ 50_days_of_python.py
@@ -0,0 +1,14 @@
+# Day 2. Convert strings to integers
+convert_add = ['1', '3', '5']
+new_list = []
+
+for item in convert_add:
+    new_list.append(int(item))
+
+print(convert_add)
+print(new_list)
+print(sum(new_list)) 
+
+# Write a function that takes a list of names and returns the duplicates otherwise return no duplicates
+def check_duplicates(list_duplicates):
+    pass
