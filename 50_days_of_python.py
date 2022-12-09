fc490430a3140c15d6e35c2e3ea429cd4a5b0768
msnkr <mikyle.designs@gmail.com>
2022-12-09 14:02:33 +0200

    Update 50_days_of_python.py

--- 50_days_of_python.py
+++ 50_days_of_python.py
@@ -1,14 +1,35 @@
 # Day 2. Convert strings to integers
-convert_add = ['1', '3', '5']
-new_list = []
+# convert_add = ['1', '3', '5']
+# new_list = []
 
-for item in convert_add:
-    new_list.append(int(item))
+# for item in convert_add:
+#     new_list.append(int(item))
 
-print(convert_add)
-print(new_list)
-print(sum(new_list)) 
+# print(convert_add)
+# print(new_list)
+# print(sum(new_list)) 
 
+# Extra Challenge
 # Write a function that takes a list of names and returns the duplicates otherwise return no duplicates
-def check_duplicates(list_duplicates):
-    pass
+
+# fruits = ['apple', 'orange', 'banana', 'apple']
+# numbers = ['1', '2', '2', '3']
+# names = ['Yoda', 'Moses', 'Joshua', 'Mark']
+# non_duplicates = []
+
+# def check_dupliacates(list_items):
+#     for item in list_items:
+#         if item not in non_duplicates:
+#             non_duplicates.append(item)
+#         else:
+#             print(item)
+#             return False
+
+
+# check = check_dupliacates(names)
+# if check != False:
+#     print('There are no duplicates')
+
+
+##############################################################################################
+# Day 3. 
