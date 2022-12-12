1157abda3911609ac7389e5f257d1e20a4587346
msnkr <mikyle.designs@gmail.com>
2022-12-12 23:16:07 +0200

    .

--- Day 24/Mail Merge Project Start/main.py
+++ Day 24/Mail Merge Project Start/main.py
@@ -6,3 +6,15 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open('Day 24/Mail Merge Project Start/Input/Names/invited_names.txt')as file:
+    names_list = file.readlines()
+
+with open('Day 24/Mail Merge Project Start/Input/Letters/starting_letter.txt')as file:
+    letter = file.read()
+
+for name in names_list:
+    with open(f'Day 24/Mail Merge Project Start/Output/ReadyToSend/{name}.txt', 'w')as file:
+        new_name = name.rstrip()
+        file.write(letter.replace('[name]', new_name))
