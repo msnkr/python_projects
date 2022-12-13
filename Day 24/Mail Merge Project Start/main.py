d9195cbb651fe50d1cd4c0940261b818cb99fd70
msnkr <mikyle.designs@gmail.com>
2022-12-13 12:30:42 +0200

    .

--- Day 24/Mail Merge Project Start/main.py
+++ Day 24/Mail Merge Project Start/main.py
@@ -8,13 +8,21 @@
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 
-with open('Day 24/Mail Merge Project Start/Input/Names/invited_names.txt')as file:
-    names_list = file.readlines()
+with open(r'.\Day 24\Mail Merge Project Start\Input\Names\invited_names.txt')as names:
+    name_content = names.readlines()
 
-with open('Day 24/Mail Merge Project Start/Input/Letters/starting_letter.txt')as file:
-    letter = file.read()
 
-for name in names_list:
-    with open(f'Day 24/Mail Merge Project Start/Output/ReadyToSend/{name}.txt', 'w')as file:
-        new_name = name.rstrip()
-        file.write(letter.replace('[name]', new_name))
+with open(r'.\Day 24\Mail Merge Project Start\Input\Letters\starting_letter.txt')as letter:
+    letter_content = letter.read()
+
+
+
+for name in name_content:
+    with open (f'.\Day 24\Mail Merge Project Start\Output\LettersToSend\{name}.txt', 'w') as name_letter:
+        strip_name = name.strip()
+        new_letter = letter_content.replace('[name]', strip_name)
+        name_letter.write(new_letter)
+        
+
+
+        
