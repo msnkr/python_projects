94b84124fdf0c45785ba9291595354784cba6660
msnkr <mikyle.designs@gmail.com>
2022-12-26 23:55:29 +0200

    .

--- Day 25/Us States Start/main.py
+++ Day 25/Us States Start/main.py
@@ -22,14 +22,17 @@ turtle = Turtle(shape=image)
 
 
 # 2. Create new text.
-# game_on = True
-# while game_on:
-#     answer = screen.textinput(title='Guess the States', prompt='Name another State: ').capitalize()
-#     # 1. Find answer in row
-#     states = data.state == answer
-#     if states == True:
-#         pass
+game_on = True
+while game_on:
+    answer = screen.textinput(title='Guess the States', prompt='Name another State: ').capitalize()
+    # 1. Find answer in row
+    states = data[data.state == answer]
+    states_x = states.x
+    states_y = states.y
 
+    # turtle = Turtle(shape='square')
+    # turtle.goto(states_x, states_y)
+    print(f'{states_x}')
 
 # 3. Get x and Y coordinates from csv
 # turtle goto with x and y coordinates
