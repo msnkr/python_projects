fba81e02820b8ac682ff0b4cc10cc68512a3ff7d
msnkr <mikyle.designs@gmail.com>
2022-12-28 23:42:52 +0200

    .

--- Day 25/Us States Start/main.py
+++ Day 25/Us States Start/main.py
@@ -22,26 +22,33 @@ turtle = Turtle(shape=image)
 
 
 # 2. Create new text.
-game_on = True
 is_correct = 0
-while game_on:
+guessed_states = []
+
+while is_correct != 50:
     answer = screen.textinput(title='Guess the States', prompt=f'Name another State: ({is_correct}/50)').title()
-    if answer != '':
-        # 1. Find answer in row.
-        states = data[data['state'] == answer]
+    # 1. Find answer in row.
+    states = data[data['state'] == answer]
+    if answer == 'Exit':
+        to_guess = []
         for row in data.state:
-            if row == answer:
-                # 3. Create new turtle object to write data to map
-                turtle = Turtle()
-                turtle.speed(1)
-                turtle.penup()
-                # 4. Turtle goto location on map
-                turtle.goto(float(states.x), float(states.y))
-                turtle.write(f'{answer}', align='center', font=('Arial', 10, 'normal'))
-                is_correct += 1
-                turtle.hideturtle()
+            if row not in guessed_states:
+                to_guess.append(row)
 
+        df = pandas.DataFrame(to_guess)
+        df.to_csv('./Day 25/Us States Start/to_guess.csv')
+        break
+    
+    for row in data.state:
+        if row == answer:
+            # 3. Create new turtle object to write data to map
+            turtle = Turtle()
+            turtle.speed(1)
+            turtle.penup()
+            # 4. Turtle goto location on map
+            turtle.goto(float(states.x), float(states.y))
+            turtle.write(f'{answer}', align='center', font=('Arial', 10, 'normal'))
+            is_correct += 1
+            turtle.hideturtle()
+            guessed_states.append(states.state.item())
 
-screen.exitonclick()
-
-# Fix that name moves to goto
---
+++ Day 26/start.py
@@ -0,0 +1,42 @@
+# List comprehension
+# For loop.
+# num_list = [1, 2, 3]
+# new_num_list = []
+# for n in num_list:
+#     add_n = n + 1
+#     new_num_list.append(add_n)
+
+# print(new_num_list)
+
+
+# List == list to itirate through.
+# Item is each item in the list.
+# New_list is code execution
+# new_list = [new_item for item in list]
+
+# numbers = [1, 2, 3]
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+
+# numbers = [1, 2, 3]
+# test_list = []
+# new_list = [test_list.append(n + 2) for n in numbers]
+
+# print(test_list)
+
+# List comprehension can be any sequence - Lists, tuples, Dictionaries, Strings
+# name = 'Mikyle'
+# new_list = [letter for letter in name]
+# print(new_list)
+
+# range_list = [number * 2 for number in range(1, 5)]
+# print(range_list)
+
+# Conditional list comprehension
+# names = ['Mikyle', 'Kashia', 'Lara', 'Marcus', 'Selene', 'Kash', 'Angelica']
+# names_list = [name for name in names if len(name) < 5]
+# print(names_list)
+
+names = ['Mikyle', 'Lara', 'Marcus', 'Selene', 'Kash', 'Angelica']
+names_list = [name.upper() for name in names if len(name) > 5]
+print(names_list)
