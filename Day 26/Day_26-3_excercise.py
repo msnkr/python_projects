2c80a63f6bc3fda89f977d078cf53ddda935d5ba
msnkr <mikyle.designs@gmail.com>
2022-12-29 21:50:12 +0200

    .

--- Day 26/Day_26-3_excercise.py
+++ Day 26/Day_26-3_excercise.py
@@ -1,13 +1,7 @@
 
-with open('./Day 26/file1.txt')as f1:
-    f1.read()
-
-with open('./Day 26/file2.txt')as f2:
-    f2.read()
-
-result = [new_item for item in list if test]
 
 # Write your code above 👆
+
 
 print(result)
 
