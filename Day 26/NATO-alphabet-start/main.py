2d9a21960c86a25cefeaff357a55b987e38f377b
msnkr <mikyle.designs@gmail.com>
2022-12-30 23:17:43 +0200

    .

--- Day 26/NATO-alphabet-start/main.py
+++ Day 26/NATO-alphabet-start/main.py
@@ -7,10 +7,18 @@ import pandas
 nato_data = pandas.read_csv('./Day 26/NATO-alphabet-start/nato_phonetic_alphabet.csv')
 nato_dict = {row.letter:row.code for (index, row) in nato_data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+game = True
+while game:
+    #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+    # Get the input into a variable.
+    # Convert each letter in variable into a list using split or splitlines.
 
-# Get the input into a variable.
-# Convert each letter in variable into a list using split or splitlines.
-user_input = list(input('Enter Word: ').lower())
-# Loop thorough each item in the variable list to print out the letters associated with the dictionary
+    user_input = list(input('Enter Word: ').upper())
+    if user_input == ['E', 'X', 'I', 'T']:
+        break
 
+    # Loop thorough each item in the variable list to print out the letters associated with the dictionary
+    # nato_output = [new_item for word in user_input]
+
+    nato_words = [nato_dict[letter] for letter in user_input]
+    print(nato_words)
