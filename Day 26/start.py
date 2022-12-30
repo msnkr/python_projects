6c1da7bf98797dd95f74ab066a303ec4500003f8
msnkr <mikyle.designs@gmail.com>
2022-12-30 22:22:47 +0200

    .

--- Day 26/start.py
+++ Day 26/start.py
@@ -1,4 +1,5 @@
 # List comprehension
+####################################################################
 # For loop.
 # num_list = [1, 2, 3]
 # new_num_list = []
@@ -8,21 +9,29 @@
 
 # print(new_num_list)
 
+####################################################################
 
 # List == list to itirate through.
 # Item is each item in the list.
 # New_list is code execution
 # new_list = [new_item for item in list]
 
+####################################################################
+
+
 # numbers = [1, 2, 3]
 # new_list = [n + 1 for n in numbers]
 # print(new_list)
+
+####################################################################
 
 # numbers = [1, 2, 3]
 # test_list = []
 # new_list = [test_list.append(n + 2) for n in numbers]
 
 # print(test_list)
+
+####################################################################
 
 # List comprehension can be any sequence - Lists, tuples, Dictionaries, Strings
 # name = 'Mikyle'
@@ -31,6 +40,8 @@
 
 # range_list = [number * 2 for number in range(1, 5)]
 # print(range_list)
+
+####################################################################
 
 # Conditional list comprehension
 # names = ['Mikyle', 'Kashia', 'Lara', 'Marcus', 'Selene', 'Kash', 'Angelica']
@@ -41,14 +52,65 @@
 # names_list = [name.upper() for name in names if len(name) > 5]
 # print(names_list)
 
+####################################################################
+
 # Dictionary Comprehension
-import random
-names = ['Mikyle', 'Kashia', 'Lara', 'Marcus', 'Selene', 'Kash', 'Angelica']
+# import random
+# names = ['Mikyle', 'Kashia', 'Lara', 'Marcus', 'Selene', 'Kash', 'Angelica']
 
-# student_dict = {key:value for item in dict}
-student_dict = {student:random.randint(1, 100) for student in names}
-print(student_dict)
+# # student_dict = {key:value for item in list}
+# student_dict = {name:random.randint(1, 100) for name in names}
+# print(student_dict)
 
-# passed_students = {new_key:new_value for (key:value) in dict.items() if test}
+####################################################################
 
+# # passed_students = {new_key:new_value for (key:value) in dict.items() if test}
+# passed_students = {student:score for (student, score) in student_dict.items() if score >= 50}
+# print(passed_students)
+
+####################################################################
+
+# Looping through dictionaries
+# student_dict = {
+#     'Student': ['Mikyle', 'Lara', 'Kashi'],
+#     'Score': [78, 99, 98]
+# }
+
+# for (key, value) in student_dict.items():
+#     print(f'{key}: {value}')
+
+####################################################################
+
+# Looping through items in pandas
+# import pandas
+
+# student_dict = {
+#     'Student': ['Mikyle', 'Lara', 'Kashi'],
+#     'Score': [78, 99, 98]
+# }
+
+# student_df = pandas.DataFrame(student_dict)
+# print(student_df)
+# This is not the best way.
+# for (key, value) in student_df.items():
+#     print(value)
+
+####################################################################
+
+# Loop through a dataframe
+import pandas
+
+student_dict = {
+    'Student': ['Mikyle', 'Lara', 'Kashi'],
+    'Score': [78, 99, 98]
+}
+student_df = pandas.DataFrame(student_dict)
+# print(student_df)
+for (index, row) in student_df.iterrows():
+    # print(row)
+    # Or
+    # print(row.Student)
+    # And this returns a dataframe series
+    if row.Student == 'Kashi':
+        print(f"{row.Student}: {row.Score}")
 
