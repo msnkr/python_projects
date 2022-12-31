15d6811b98abc789c21a05b2fdc4ff37c76a2644
msnkr <mikyle.designs@gmail.com>
2022-12-31 22:08:39 +0200

    .

--- Day 27/playground.py
+++ Day 27/playground.py
@@ -23,15 +23,42 @@
 # add(1, 1, 7, 8, 11)
 
 # kwargs is multiple keyword arguments. It saves all arguments in a dictionary where you can call each by its key
+# def calculate(a, b, **kwargs):
+#     # print(kwargs)
+#     # print(kwargs['add'])
+#     # for key, value in kwargs.items():
+#     #     print(f'{key}: {value}')
+#     a += kwargs['add']
+#     b *= kwargs['multiply']
+#     print(a, b)
 
-def calculate(a, b, **kwargs):
-    # print(kwargs)
-    # print(kwargs['add'])
-    # for key, value in kwargs.items():
-    #     print(f'{key}: {value}')
-    a += kwargs['add']
-    b *= kwargs['multiply']
-    print(a, b)
+
+# calculate(a=3, b=5, add=5, multiply=2)
+
+# Create your own kwargs class which workks the same.
+
+# class Car:
+
+#     def __init__(self, **kwargs):
+#         self.make = kwargs['make']
+#         self.model = kwargs['model']
 
 
-calculate(a=3, b=5, add=5, multiply=2)
+# my_car = Car(make='Nissan', model='GT-R')
+# print(my_car.model)
+# But if you don't pass in a model you will get an error because it can't find it.
+# You can use the model called .get which will return none if it cant find a keyword argument.
+
+class Car:
+
+    def __init__(self, **kwargs):
+        self.make = kwargs.get('make')
+        self.model = kwargs.get('model')
+        self.color = kwargs.get('color')
+        self.year = kwargs.get('year')
+
+
+my_car = Car(make='Nissan')
+print(my_car.model)
+
+# Technivcally, kwargs are optional.Check documentation on all modules
--- Day 27/start.py
+++ Day 27/start.py
@@ -1,10 +1,31 @@
-# import tkinter
+from tkinter import *
+import time
 
-# tk = tkinter.Tk()
+window = Tk()
 
-# tk.title('Gui')
-# my_label = tkinter.Label(text='This is a label.', font=('Hack', 24))
-# my_label.pack()
+window.title('Gui')
+my_label = Label(text='This is a label.', font=('Hack', 24))
+# You can set options using the set command.
+# my_label.config(text='New Text')
+# Or
+# my_label['text'] = 'This is new text'
 
-# tk.mainloop()
+my_label.pack()
 
+# Eveent listeners
+def button_clicked():
+    my_label.config(text='I got clicked')
+
+# Buttons
+new_button = Button()
+new_button.config(bg='teal', fg='white', text='Hello. I\'m a button', command=button_clicked)
+new_button.pack()
+
+# Data entry
+user_entry = Entry()
+user_input = user_entry.get()
+user_entry.pack()
+
+
+
+window.mainloop()
