03bd185a71726ba6db02447fee1a4141a9d5604e
msnkr <mikyle.designs@gmail.com>
2023-01-02 20:04:46 +0200

    .

---
+++ Day 27/miles_to_km_converter.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+window.title('Miles to KM converter')
+window.config(padx=100, pady=100)
+
+# 3 Rows and 3 columns
+# Create a calculate button
+# Caluculate function must get data input, * input by 1.6 and change label to answer
+
+
+
+window.mainloop()
--- Day 27/start.py
+++ Day 27/start.py
@@ -4,6 +4,7 @@ import time
 window = Tk()
 
 window.title('Gui')
+window.config(padx=50, pady=50)
 # my_label = Label(text='This is a label.', font=('Hack', 24))
 # # You can set options using the set command.
 # # my_label.config(text='New Text')
@@ -25,6 +26,8 @@ window.title('Gui')
 # user_entry = Entry(width=10)
 # user_entry.pack()
 
+#  Layout managers pack, place(x=, y=) and grid
+
 def listen_for_button():
     """
     Change label
@@ -34,16 +37,18 @@ def listen_for_button():
 
 # Label
 new_label = Label(text='This is a label.', font=('Hack', 22))
-new_label.pack()
+new_label.config(padx=10, pady=10)
+new_label.grid(column=0, row=0)
 
 # Data
 text_data = Entry(width=25)
-text_data
-text_data.pack()
+text_data.grid(column=4, row=3)
 
 # Button
 button = Button(text='Click me to change the label', font=('Hack', 16), fg='white', bg='black', command=listen_for_button)
-button.pack()
+button.grid(column=1, row=1)
 
+new_button = Button(text='New button', fg='white', bg='black')
+new_button.grid(column=2, row=0)
 window.mainloop()
 
