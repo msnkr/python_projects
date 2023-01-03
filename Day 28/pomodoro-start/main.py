ba268c74f59333be9120012368d812cc15350fac
msnkr <mikyle.designs@gmail.com>
2023-01-03 23:37:46 +0200

    .

--- Day 28/pomodoro-start/main.py
+++ Day 28/pomodoro-start/main.py
@@ -1,31 +1,33 @@
 from tkinter import *
-import math
+from math import floor
 from PIL import ImageTk, Image
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Source Code Pro"
-WORK_MIN = 25
+FONT_NAME = "Josefin Sans"
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
 def start_timer():
     count_down(5)
 
-# ---------------------------- UI SETUP ------------------------------- #
-
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_minutes = floor(count / 60)
+    count_seconds = count % 60
+    if count_seconds < 10:
+        count_seconds = f'0{count_seconds}'
+        
+    canvas.itemconfig(timer_text, text=f'{count_minutes}:{count_seconds}')
     if count > 0:
         window.after(1000, count_down, count - 1)
 
+# ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title('Pomodoro')
