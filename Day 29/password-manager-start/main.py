1efd3341d4bac201296d129e666ee7b30726f599
msnkr <mikyle.designs@gmail.com>
2023-01-06 23:07:39 +0200

    .

--- Day 29/password-manager-start/main.py
+++ Day 29/password-manager-start/main.py
@@ -1,12 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
+from random import randint, choice, shuffle
+import pyperclip
+
 
 WHITE = 'white'
 FONT = ('Iosveka', 12, 'bold')
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+
+    shuffle(password_list)
+    password = ''.join(password_list)
+    
+    pyperclip.copy(password)
+    password_entry.insert(0, password)
+    
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
@@ -61,13 +79,10 @@ password_entry = Entry()
 password_entry.grid(column=1, row=4, sticky='EW')
 
 # Button
-generate_pass_button = Button(text='Generate Password', bg=WHITE, font=('Iosveka', 8))
+generate_pass_button = Button(text='Generate Password', bg=WHITE, font=('Iosveka', 8), command=generate_password)
 generate_pass_button.grid(column=2, row=4, sticky='EW')
 
 add_button = Button(text='Add', bg=WHITE, font=('Iosveka', 8), width=36, command=save)
 add_button.grid(column=1, row=5, columnspan=4, sticky='EW')
-
-
-
 
 window.mainloop()
