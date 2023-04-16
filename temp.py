aa2a2234b816e5b1daf1493ea1304166b57c0abf
msnkr <mikyle.designs@gmail.com>
2023-04-16 21:20:52 +0200

    .

--- temp.py
+++ temp.py
@@ -1,5 +1,34 @@
-# /usr/bin/vcgencmd measure_temp
-import os 
+import os
 
-temp = os.system('/usr/bin/vcgencmd measure_temp')
-print(temp)
+
+def main():
+    """
+    Program to demonstrate how to obtain the current value of the CPU temperature.
+    """
+    print('Current CPU temperature is {:.2f} degrees Celsius.'.format(
+        get_cpu_temp()))
+
+
+def get_cpu_temp():
+    """
+    Obtains the current value of the CPU temperature.
+    :returns: Current value of the CPU temperature if successful, zero value otherwise.
+    :rtype: float
+    """
+    # Initialize the result.
+    result = 0.0
+    # The first line in this file holds the CPU temperature as an integer times 1000.
+    # Read the first line and remove the newline character at the end of the string.
+    if os.path.isfile('/sys/class/thermal/thermal_zone0/temp'):
+        with open('/sys/class/thermal/thermal_zone0/temp') as f:
+            line = f.readline().strip()
+        # Test if the string is an integer as expected.
+        if line.isdigit():
+            # Convert the string with the CPU temperature to a float in degrees Celsius.
+            result = float(line) / 1000
+    # Give the result back to the caller.
+    return result
+
+
+if __name__ == "__main__":
+    main()
