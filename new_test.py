57847bee0d254f6900e02a3149dd7917612307cc
msnkr <mikyle.designs@gmail.com>
2023-09-12 13:09:24 +0200

    .

--- new_test.py
+++ new_test.py
@@ -1205,34 +1205,54 @@
 
 
 ############## Expense Tracker ####################
+from prettytable import PrettyTable
 
+x = PrettyTable()
 
 print("Welcome to Expense Tracker")
-print("Add income: {add}")
-print("List Items and money: {list}")
-print("Remaining Money: {remain}")
+print("Add item: {add}")
+print("Add income: {income}")
+print("List items and money: {list}")
+print("Remaining money: {remain}")
+print("Save list: {save}")
+print("Delete list item: {delete}")
 print("Exit: {exit}")
 bills = {}
+total_expense = 0
+my_income = 0
 
 
 def list_bills(bills):
+    x.clear()
+    x.field_names = ["Bill", "Money"]
     for bill_item in bills:
-        print(bill_item, bills[bill_item])
+        bill_name = bill_item
+        bill_amount = bills[bill_item]
+        x.add_row([bill_name, bill_amount])
+    print(x)
+    if my_income == 0:
+        print(f"Your total expense is {total_expense}")
+    else:
+        print(
+            f"Your total expense is {total_expense}. Your income is {my_income}. You're left with {income - total_expense}")
 
 
-# Add income
 flag = True
 while flag:
     todo = input("What do you want to do? ")
     if todo == "exit":
         flag = False
+    elif todo == "income":
+        income = int(input("Add income: "))
+        my_income += income
+    elif todo == "delete":
+        for count, item in enumerate(bills):
+            print(count+1, item)
+        delete = input("What do you want to delete? ")
     elif todo == "add":
         name_expense = input("What do you want to add?: ")
         money_expense = int(input("How much?: "))
+        total_expense += money_expense
         bills[name_expense] = money_expense
     elif todo == "list":
         list_bills(bills)
-
-
-# Add item and monies
-# Show left over monies and items
