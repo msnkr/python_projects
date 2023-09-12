9ac048ed16dc599ecd994b1b11a9f962d51c27a4
msnkr <mikyle.designs@gmail.com>
2023-09-12 09:51:58 +0200

    .

--- Day 31/redo/test.py
+++ Day 31/redo/test.py
@@ -7,16 +7,16 @@ root = Tk()
 
 root.title("Hello")
 frame = ttk.Frame(root, padding=20)
-frame.grid()
+frame.pack(fill="both", expand=True, padx=8, pady=8)
 
 
 def hello_text():
     print("Hello World")
 
 
-ttk.Label(frame, text="Hello, World!").grid(column=0, row=0)
-ttk.Button(frame, text="Click Here", command=hello_text).grid(column=0, row=1)
-ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=1)
+ttk.Label(frame, text="Hello, World!").pack(fill="both")
+ttk.Button(frame, text="Click Here", command=hello_text).pack(fill="both")
+ttk.Button(frame, text="Quit", command=root.destroy).pack(fill="both")
 
 
 root.mainloop()
