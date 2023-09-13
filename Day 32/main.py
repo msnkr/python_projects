0a10f471978c4b3c9c76648b0648de00c1472964
msnkr <mikyle.designs@gmail.com>
2023-09-13 15:29:41 +0200

    .

--- Day 32/main.py
+++ Day 32/main.py
@@ -9,15 +9,16 @@ with open(r"C:\Users\Digital\Documents\GitHub\python_projects\Day 32\quotes.txt"
     for file_content in file.readlines():
         quote_list.append(file_content)
 
+print(random.choice(quote_list))
 
-user = environ["email_user"]
-passwd = environ["email_pass"]
+# user = environ["email_user"]
+# passwd = environ["email_pass"]
 
 
-server = SMTP("smtp.gmail.com", 587)
-server.starttls()
+# server = SMTP("smtp.gmail.com", 587)
+# server.starttls()
 
-server.login(user, passwd)
-server.sendmail(
-    user, user, f"Subject:This is a test\n\n {random.choice(quote_list)} ")
-server.quit()
+# server.login(user, passwd)
+# server.sendmail(
+#     user, user, f"Subject:This is a test\n\n {random.choice(quote_list)} ")
+# server.quit()
