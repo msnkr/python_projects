a5d861e2f0e5acf1efc49d52a7aa8d5f23c1243c
msnkr <mikyle.designs@gmail.com>
2023-09-13 15:26:41 +0200

    .

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,5 +1,14 @@
 from smtplib import SMTP
 from os import environ
+import random
+
+
+quote_list = []
+
+with open(r"C:\Users\Digital\Documents\GitHub\python_projects\Day 32\quotes.txt", "r")as file:
+    for file_content in file.readlines():
+        quote_list.append(file_content)
+
 
 user = environ["email_user"]
 passwd = environ["email_pass"]
@@ -9,5 +18,6 @@ server = SMTP("smtp.gmail.com", 587)
 server.starttls()
 
 server.login(user, passwd)
-server.sendmail(user, user, "Subject:This is a test\n\n How are you? ")
+server.sendmail(
+    user, user, f"Subject:This is a test\n\n {random.choice(quote_list)} ")
 server.quit()
