b5d6b5e5265ecb917664be32580f61ade0be1d28
msnkr <mikyle.designs@gmail.com>
2023-09-13 09:51:19 +0200

    .

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,1 +1,13 @@
-import smtplib
+from smtplib import SMTP
+from os import environ
+
+user = environ["email_user"]
+passwd = environ["email_pass"]
+
+
+server = SMTP("smtp.gmail.com", 587)
+server.starttls()
+
+server.login(user, passwd)
+server.sendmail(user, user, "Subject:This is a test\n\n How are you? ")
+server.quit()
