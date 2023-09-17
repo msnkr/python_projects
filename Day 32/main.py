bcc1848abee8488d3c9b949540c5ef89de886e85
msnkr <mikyle.designs@gmail.com>
2023-09-17 22:36:46 +0200

    .

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,15 +1,20 @@
 from smtplib import SMTP
 from os import environ
 import random
+import datetime as dt
 
 
-quote_list = []
+# now = dt.datetime.now()
+# print(now.isoweekday())
 
-with open(r"C:\Users\Digital\Documents\GitHub\python_projects\Day 32\quotes.txt", "r")as file:
-    for file_content in file.readlines():
-        quote_list.append(file_content)
 
-print(random.choice(quote_list))
+# quote_list = []
+
+# with open(r"C:\Users\mikyl\Documents\Git\python_projects\Day 32\quotes.txt", "r")as file:
+#     for file_content in file.readlines():
+#         quote_list.append(file_content)
+
+# print(random.choice(quote_list))
 
 # user = environ["email_user"]
 # passwd = environ["email_pass"]
@@ -22,3 +27,27 @@ print(random.choice(quote_list))
 # server.sendmail(
 #     user, user, f"Subject:This is a test\n\n {random.choice(quote_list)} ")
 # server.quit()
+
+
+# Get random quote from quotes.txt
+# If datetime is the current day, get a random quote
+# Email random quote
+
+
+random_quotes = []
+now_date = dt.datetime.now()
+user_email = environ["email_user"]
+user_pass = environ["email_pass"]
+
+
+if now_date.isoweekday() == 7:
+    with open(r"C:\Users\mikyl\Documents\Git\python_projects\Day 32\quotes.txt")as quote:
+        all_quotes = quote.readlines()
+        random_quote = random.choice(all_quotes)
+
+    with SMTP("smtp.gmail.com", 587)as server:
+        server.starttls()
+
+        server.login(user_email, user_pass)
+        server.sendmail(user_email, user_email,
+                        f"Subject:Quote of the day \n\n {random_quote}")
