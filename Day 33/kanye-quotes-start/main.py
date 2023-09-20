3f2d1087ff96e76db087762f735e017fb8cc4ca5
msnkr <mikyle.designs@gmail.com>
2023-09-20 22:26:21 +0200

    .

--- Day 33/kanye-quotes-start/main.py
+++ Day 33/kanye-quotes-start/main.py
@@ -5,9 +5,9 @@ import requests
 
 def get_quote():
     response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
     json_response = response.json()
-    print(json_response["quote"])
-    return json_response["quote"]
+    canvas.itemconfig(quote_text, text=json_response["quote"])
 
 
 window = Tk()
@@ -17,14 +17,14 @@ window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(
-    file=r"C:\Users\Digital\Documents\GitHub\python_projects\Day 33\kanye-quotes-start\background.png")
+    file=r"Day 33/kanye-quotes-start/background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text=get_quote(), width=250, font=(
+quote_text = canvas.create_text(150, 207, text="Text goes here", width=250, font=(
     "Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(
-    file=r"C:\Users\Digital\Documents\GitHub\python_projects\Day 33\kanye-quotes-start\kanye.png")
+    file=r"C:\Users\mikyl\Documents\Git\python_projects\Day 33\kanye-quotes-start\kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
--- Day 33/main.py
+++ Day 33/main.py
@@ -1,11 +1,37 @@
 import requests
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
+# response = requests.get("http://api.open-notify.org/iss-now.json")
+
+# if response.ok:
+#     data = response.json()
+#     iss_position = data["iss_position"]
+#     position = {data: iss_position[data] for data in iss_position}
+
+#     print(position["longitude"])
+#     print(position["latitude"])
+
+MY_LAT = -26.204103
+MY_LNG = 28.047304
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0,
+}
+
+
+def split_data(data):
+    return data.split("T")[1].split(":")[0]
+
+
+response = requests.get(
+    "https://api.sunrise-sunset.org/json", params=parameters)
 
 if response.ok:
-    data = response.json()
-    iss_position = data["iss_position"]
-    position = {data: iss_position[data] for data in iss_position}
+    data = response.json()["results"]
+    sunrise = data["sunrise"]
+    sunset = data["sunset"]
 
-    print(position["longitude"])
-    print(position["latitude"])
+    sunrise_time = split_data(sunrise)
+    sunset_time = split_data(sunset)
+
+    print(sunrise_time)
