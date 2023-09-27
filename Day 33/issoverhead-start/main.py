5388210c7d21b43e175e4a1cc98bf22e4bd7fe9e
msnkr <mikyle.designs@gmail.com>
2023-09-27 22:11:45 +0200

    .

--- Day 33/issoverhead-start/main.py
+++ Day 33/issoverhead-start/main.py
@@ -2,13 +2,15 @@ import requests
 from datetime import datetime
 from smtplib import SMTP
 import os
+import time
 
 
-MY_EMAIL = os.environ("email_user")
-MY_PASS = os.environ("email_pass")
+# MY_EMAIL = os.environ("email_user")
+# MY_PASS = os.environ("email_pass")
 
 MY_LAT = -26.204103
 MY_LNG = 28.047304
+
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -18,12 +20,11 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
 
-# Your position is within +5 or -5 degrees of the ISS position.
 def is_close(MY_LAT, MY_LNG, iss_latitude, iss_longitude):
     if MY_LAT == iss_latitude + 5 or MY_LAT == iss_latitude - 5 and MY_LNG == iss_longitude + 5 or MY_LNG == iss_longitude - 5:
-        return False
-    else:
         return True
+    else:
+        return False
 
 
 parameters = {
@@ -40,18 +41,24 @@ sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now().hour
-if time_now > sunset and time_now < sunrise:
-    if is_close(MY_LAT, MY_LNG, iss_latitude, iss_longitude):
-        with SMTP("smtp.gmail.com")as server:
 
-            server.starttls()
-            server.connect(MY_EMAIL, MY_PASS)
-            server.sendmail(
-                MY_EMAIL, MY_EMAIL, msg="Subject: Look Up bro \n\n Look to the sky for the ISS satelite bro. ")
+while True:
+    if time_now > sunset or time_now < sunrise:
+        if is_close(MY_LAT, MY_LNG, iss_latitude, iss_longitude):
+            print("Email sent")
+            # with SMTP("smtp.gmail.com")as server:
+
+            #     server.starttls()
+            #     server.connect(MY_EMAIL, MY_PASS)
+            #     server.sendmail(
+            #         MY_EMAIL, MY_EMAIL, msg="Subject: Look Up bro \n\n Look to the sky for the ISS satelite bro. ")
+        else:
+            print("Not here")
     else:
-        print("Not here")
-else:
-    print("Not time")
+        print("Not time")
+    time.sleep(60)
+
+
 # If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
