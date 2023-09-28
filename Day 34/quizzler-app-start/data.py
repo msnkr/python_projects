7b5e981d992c6e18666abcb1172067858cee545b
msnkr <mikyle.designs@gmail.com>
2023-09-28 22:17:34 +0200

    .

--- Day 34/quizzler-app-start/data.py
+++ Day 34/quizzler-app-start/data.py
@@ -3,18 +3,15 @@ import requests
 params = {
     "amount": 10,
     "difficulty": "easy",
-    "type": "boolean"
+    "type": "boolean",
 }
 
 link = "https://opentdb.com/api.php?"
-question_data = {}
+question_data = []
 
 response = requests.get(link, params=params)
 
 if response.ok:
     data = response.json()["results"]
     for question in data:
-        question_data["question"] = question["question"]
-        question_data["correct_answer"] = question["correct_answer"]
-
-print(question_data)
+        question_data.append(question)
--- Day 34/quizzler-app-start/quiz_brain.py
+++ Day 34/quizzler-app-start/quiz_brain.py
@@ -12,7 +12,8 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
+        user_answer = input(
+            f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
         self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
