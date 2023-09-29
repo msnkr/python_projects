815c6448fe050f8f7c4a0ac4519bbe17e900249f
msnkr <mikyle.designs@gmail.com>
2023-09-29 12:46:55 +0200

    .

--- new_test.py
+++ new_test.py
@@ -1326,30 +1326,107 @@
 
 # window.mainloop()
 
-import tkinter as tk
+# import tkinter as tk
 
-root = tk.Tk()
-root.title("SHITR|")
+# root = tk.Tk()
+# root.title("SHITR|")
 
-root.config(width=200, height=200, padx=20, pady=20)
-
-
-def calculate():
-    num1 = int(number1.get())
-    num2 = int(number2.get())
-
-    label.config(text=num1+num2)
+# root.config(width=200, height=200, padx=20, pady=20)
 
 
-label = tk.Label(root, text="Test")
-label.pack()
+# def calculate():
+#     num1 = int(number1.get())
+#     num2 = int(number2.get())
 
-number1 = tk.Entry(root)
-number1.pack()
+#     label.config(text=num1+num2)
 
-number2 = tk.Entry(root)
-number2.pack()
 
-button = tk.Button(root, text="calculate", command=calculate)
-button.pack()
-root.mainloop()
+# label = tk.Label(root, text="Test")
+# label.pack()
+
+# number1 = tk.Entry(root)
+# number1.pack()
+
+# number2 = tk.Entry(root)
+# number2.pack()
+
+# button = tk.Button(root, text="calculate", command=calculate)
+# button.pack()
+# root.mainloop()
+
+
+# class Animal():
+#     def __init__(self, name, age, species):
+#         self.name = name
+#         self.age = age
+#         self.species = species
+
+#     def print_animal(self):
+#         print(self.name, self.age, self.species)
+
+
+# tiger = Animal("tiger", 6, "dog")
+# tiger.print_animal()
+
+
+class Library():
+    def __init__(self):
+        self.books = []
+
+    def add_books(self, book):
+        self.books.append(book)
+
+    def remove_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+                return True
+        return False
+
+    def list_books(self):
+        if not self.books:
+            print("The library is empty")
+        else:
+            print("Library catalag")
+            for i, book in enumerate(self.books):
+                print(f"{i}. Title: {book.title}, Author: {book.author}")
+
+
+class Book():
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
+
+
+def main():
+    library = Library()
+
+    while True:
+
+        print("1. Add a book: ")
+        print("2. View all books: ")
+        print("3. Delete a book: ")
+        print("4. Quit: ")
+
+        choice = int(input("Make a choice: "))
+
+        if choice == 1:
+            author = input("Authors name: ")
+            title = input("Title name: ")
+
+            book = Book(author, title)
+            library.add_books(book)
+
+        elif choice == 2:
+            library.list_books()
+
+        elif choice == 3:
+            title = 
+            library.remove_book()
+
+        elif choice == 4:
+            print("Goodbye")
+            break
+
+
+if __name__ == "__
