72e5f51c5dbf28598345ce7cceb7aff0f1c28de1
msnkr <mikyle.designs@gmail.com>
2023-10-05 11:06:22 +0200

    .

--- torrent_search_2/main.py
+++ torrent_search_2/main.py
@@ -4,28 +4,41 @@ from tpblite import TPB, CATEGORIES
 
 
 t = TPB('https://tpb.party')
+results_dict = {}
+magnet = ""
 
 
-# def results():
-#     search = input("What do you want to search for?: ")
-#     torrents = t.search(
-#         f"{search}", category=CATEGORIES.VIDEO.MOVIES)
-#     results = {}
-#     magnet = ""
+def show_options(results):
+    canvas = Canvas(window, width=560, height=200, bg="white")
+    canvas.pack()
 
-#     for index, torrent in enumerate(torrents):
-#         results[index] = torrent
-
-#     for key in results:
-#         print(key + 1,  results[key].title)
-
-#     selection = int(input("Which one? "))
-#     for key in results:
-#         if selection == key:
-#             magnet = results[key].magnetlink
-
-#     return magnet
+    for torrent in results:
+        pass
 
 
-# subprocess.call(
-#     [r"c:\Users\Digital\AppData\Roaming\npm\webtorrent.cmd", "--vlc", results()])
+def results():
+    search = search_entry.get()
+    torrents = t.search(
+        f"{search}", category=CATEGORIES.VIDEO.MOVIES)
+
+    for index, torrent in enumerate(torrents):
+        results_dict[index] = torrent
+
+    show_options(results_dict)
+
+
+window = Tk()
+window.title("Uggh")
+window.config(width=600, height=400, padx=20, pady=20)
+
+search_label = Label(window, text="Search")
+search_label.pack()
+
+search_entry = Entry(window)
+search_entry.pack()
+
+search_btn = Button(window, text="Search", command=results)
+search_btn.pack(padx=5, pady=5)
+
+
+window.mainloop()
