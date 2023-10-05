6a2e0dca4ab1ef881deb00f774635ef0ccbaa527
msnkr <mikyle.designs@gmail.com>
2023-10-05 09:54:29 +0200

    .

--- torrent_search_2/main.py
+++ torrent_search_2/main.py
@@ -1,29 +1,31 @@
 from tkinter import *
+import subprocess
 from tpblite import TPB, CATEGORIES
 
 
 t = TPB('https://tpb.party')
 
 
-def results():
-    search = input("What do you want to search for?: ")
-    torrents = t.search( 
-        f"{search}", category=CATEGORIES.VIDEO.MOVIES)
-    results = {}
-    magnet = ""
+# def results():
+#     search = input("What do you want to search for?: ")
+#     torrents = t.search(
+#         f"{search}", category=CATEGORIES.VIDEO.MOVIES)
+#     results = {}
+#     magnet = ""
 
-    for index, torrent in enumerate(torrents):
-        results[index + 1] = torrent
+#     for index, torrent in enumerate(torrents):
+#         results[index] = torrent
 
-    for key in results:
-        print(key + 1,  results[key].title)
+#     for key in results:
+#         print(key + 1,  results[key].title)
 
-    selection = int(input("Which one? "))
-    for key in results:
-        if selection == key:
-            magnet = results[key].magnetlink
+#     selection = int(input("Which one? "))
+#     for key in results:
+#         if selection == key:
+#             magnet = results[key].magnetlink
 
-    return magnet
+#     return magnet
 
 
-print(results())
+# subprocess.call(
+#     [r"c:\Users\Digital\AppData\Roaming\npm\webtorrent.cmd", "--vlc", results()])
