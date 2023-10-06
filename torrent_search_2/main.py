bdd698556400eb882e8cc7c3a2ca399bb186efdb
msnkr <mikyle.designs@gmail.com>
2023-10-06 15:08:03 +0200

    .

--- torrent_search_2/main.py
+++ torrent_search_2/main.py
@@ -20,15 +20,15 @@ def stream(torrent):
 
 
 def show_options(results):
-    canvas = Canvas(window, bg="white")
-    canvas.grid(row=3, column=0)
-
+    results_dict = {}
+    torrent_buttons = []
     for result in results:
         result_btn = Button(
             canvas, text=results[result].title, command=lambda: stream(results[result]))
-        result_btn.config(highlightbackground="white",
-                          highlightthickness=None, highlightcolor="white", background="white")
-        result_btn.grid(row=len(torrent_buttons), column=0,  padx=5, pady=5)
+        result_btn.config(highlightbackground=BACKGROUND_COLOR,
+                          highlightthickness=1, highlightcolor=BACKGROUND_COLOR, background="white", fg=BACKGROUND_COLOR)
+        result_btn.grid(row=len(torrent_buttons), column=0,
+                        columnspan=2, padx=5, pady=5)
         torrent_buttons.append(result_btn)
 
 
@@ -51,7 +51,7 @@ window.config(width=800, height=400, padx=10,
 
 search_label = Label(window, text="Search", fg=TEXT_COLOR,
                      background=BACKGROUND_COLOR, padx=5, pady=5)
-search_label.grid(row=0, column=0)
+search_label.grid(row=0, column=0, columnspan=2)
 
 search_entry = Entry(window, fg=TEXT_COLOR,
                      background=BACKGROUND_COLOR)
@@ -65,5 +65,9 @@ quit_btn = Button(window, text="Quit", command=window.destroy,
                   fg=TEXT_COLOR, background=BACKGROUND_COLOR, padx=10, pady=5)
 quit_btn.grid(row=2, column=1, padx=5, pady=5)
 
+canvas = Canvas(window, bg="white", width=400, height=200)
+canvas.grid(row=3, column=0, columnspan=2)
+
+window.rowconfigure(3, weight=0)
 
 window.mainloop()
