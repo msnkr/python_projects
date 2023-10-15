85b54e0fde4a94a1349c166369eb7ac7277e5088
msnkr <mikyle.designs@gmail.com>
2023-10-15 23:53:36 +0200

    .

--- passwd_man_encrypt/main.py
+++ passwd_man_encrypt/main.py
@@ -9,32 +9,15 @@
 # if they want to create a password, add the username and ask the parameters of the password
 # If they want to add a password, get the username and password
 
-def create_file():
-    with open("./secret.txt", "w")as file:
-        file.write("test")
-    print("File saved")
-    main()
-
-
-def search_file():
-    try:
-        with open("./secret.txt", "r")as file:
-            file.readlines()
-    except FileNotFoundError as err:
-        create_file()
+import json
 
 
 def write_files(user, passwd):
     name_pass_dict = {}
     name_pass_dict[user] = passwd
-    with open("./secret.txt", "r+")as file:
-        file.writelines(name_pass_dict)
-    print("The username and password were added. /n")
-    main()
 
 
 def add_user():
-    search_file()
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     write_files(username, password)
@@ -49,8 +32,6 @@ def main():
 
     if choice == "1":
         add_user()
-    elif choice == "3":
-        create_file()
 
 if __name__ == "__main__":
     main()
