f55a27f12c63403d9d985c9fc166131be3c9aa7e
msnkr <mikyle.designs@gmail.com>
2023-10-15 23:50:22 +0200

    .

--- passwd_man_encrypt/main.py
+++ passwd_man_encrypt/main.py
@@ -8,11 +8,10 @@
 # Ask user if they'd like to create  or add a password
 # if they want to create a password, add the username and ask the parameters of the password
 # If they want to add a password, get the username and password
-import random
 
 def create_file():
     with open("./secret.txt", "w")as file:
-        file.write("Your keys will be saved here")
+        file.write("test")
     print("File saved")
     main()
 
@@ -24,16 +23,27 @@ def search_file():
     except FileNotFoundError as err:
         create_file()
 
+
+def write_files(user, passwd):
+    name_pass_dict = {}
+    name_pass_dict[user] = passwd
+    with open("./secret.txt", "r+")as file:
+        file.writelines(name_pass_dict)
+    print("The username and password were added. /n")
+    main()
+
+
 def add_user():
     search_file()
     username = input("Enter your username: ")
     password = input("Enter your password: ")
+    write_files(username, password)
 
 
 def main():
-    print("Hello User. Would you like to add a password or create a password?: ")
+    print("Hello User. Would you like to add a password or create a password? /n")
     print("1. Add a username and password: ")
-    print("2. Create a username and password: ")
+    print("2. Create a password: ")
     print("3. Create a file: ")
     choice = input("Enter your choice: ")
 
