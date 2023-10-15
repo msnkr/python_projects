282c8c39971b14a167329424da2da37ae7f29b8b
msnkr <mikyle.designs@gmail.com>
2023-10-15 23:34:54 +0200

    .

--- passwd_man_encrypt/main.py
+++ passwd_man_encrypt/main.py
@@ -10,19 +10,19 @@
 # If they want to add a password, get the username and password
 import random
 
+def create_file():
+    with open("./secret.txt", "w")as file:
+        file.write("Your keys will be saved here")
+    print("File saved")
+    main()
+
 
 def search_file():
-    pass
-
-
-def generate_name():
-    random_name = ""
-    alphabet = [chr(letter) for letter in range(97, 123)]
-    for _ in range(12):
-        random_name += random.choice(alphabet)
-
-    return random_name
-
+    try:
+        with open("./secret.txt", "r")as file:
+            file.readlines()
+    except FileNotFoundError as err:
+        create_file()
 
 def add_user():
     search_file()
@@ -34,11 +34,13 @@ def main():
     print("Hello User. Would you like to add a password or create a password?: ")
     print("1. Add a username and password: ")
     print("2. Create a username and password: ")
+    print("3. Create a file: ")
     choice = input("Enter your choice: ")
 
     if choice == "1":
         add_user()
-
+    elif choice == "3":
+        create_file()
 
 if __name__ == "__main__":
     main()
