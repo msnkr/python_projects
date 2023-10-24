5d0f59147e69530578e2687b983cb00982c613eb
msnkr <mikyle.designs@gmail.com>
2023-10-24 10:00:24 +0200

    .

--- notifications/main.py
+++ notifications/main.py
@@ -1,9 +1,46 @@
+from tkinter import *
 from datetime import datetime
-from plyer import notification, temperature
+from plyer import notification
 
 
-title = "This is a title"
-message = "This is a message"
+def start():
+    task = task_name_entry.get()
+    total_time = int(time_entry.get())
+
+    seconds = (total_time * 60) * 60
+    
 
 
-notification.notify(title, message, toast=True, timeout=3)
+
+window = Tk()
+window.title("Task Manager")
+window.config(width=400, height=400, padx=10, pady=10)
+
+task_name_label = Label(window, text="What task do you want to be notified for? ")
+task_name_label.grid(column=0, row=0)
+
+task_name_entry = Entry(window)
+task_name_entry.grid(column=0, row=1)
+
+time_label = Label(window, text="How many hours? ")
+time_label.grid(column=0, row=2)
+
+time_entry = Entry(window)
+time_entry.grid(column=0, row=3)
+
+start_btn = Button(window, text="Start", command=start)
+start_btn.grid(column=0, row=4)
+
+quit_btn = Button(window, text="Quit", command=window.destroy)
+quit_btn.grid(column=1, row=4)
+
+
+window.mainloop()
+
+
+
+# title = "This is a title"
+# message = "This is a message"
+
+
+# notification.notify(title, message, toast=True, timeout=3)
