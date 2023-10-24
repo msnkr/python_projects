84c0323526cd51628607951ec514bf02c2ab6927
msnkr <mikyle.designs@gmail.com>
2023-10-24 08:42:03 +0200

    .

--- notifications/main.py
+++ notifications/main.py
@@ -1,1 +1,9 @@
-from pl
+from datetime import datetime
+from plyer import notification, temperature
+
+
+title = "This is a title"
+message = "This is a message"
+
+
+notification.notify(title, message, toast=True, timeout=3)
