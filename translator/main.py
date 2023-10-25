62f7464c110f995a0ce1f04aec5ff3dbb583c481
msnkr <mikyle.designs@gmail.com>
2023-10-25 14:00:08 +0200

    .

--- translator/main.py
+++ translator/main.py
@@ -1,25 +1,27 @@
 from tkinter import *
+from translate import Translator
+
 
 
 # window = Tk()
 # window.title("Translator")
 
-from translate import Translator
-
-def translate_text(text, target_language, source_language):
-    translator = Translator(to_lang=target_language, from_lang=source_language)
-    translation = translator.translate(text)
-    return translation
-
-if __name__ == "__main__":
-    source_text = "c’est ce que c’est"
-    target_language = "en"
-    source_language = "fr"
-    
-    translated_text = translate_text(source_text, target_language, source_language)
-    print(f"Translation: {translated_text}")
-
-
-
 
 # window.mainloop()
+
+
+
+# translator = Translator(to_lang="zh")
+# translation = translator.translate("This is a pen")
+
+# print(translation)
+
+
+to_lang = input("Language Code?: ")
+text_to_translate = input("What do you want to translate?: ")
+
+
+translator = Translator(to_lang=to_lang)
+translation = translator.translate(text_to_translate)
+
+print(translation)
