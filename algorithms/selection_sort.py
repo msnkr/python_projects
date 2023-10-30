08e9ebaf67f941a92391f72fc1830195baf07013
msnkr <mikyle.designs@gmail.com>
2023-10-30 20:31:58 +0200

    .

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,13 +1,30 @@
-def selection_sort(arr):
-    for i in range(0, len(arr) - 1):
-        idx = i
-        for j in range(i + 1, len(arr)):
-            if arr[j] < arr[idx]:
-                idx = j
+# def selection_sort(arr):
+#     for i in range(0, len(arr) - 1):
+#         idx = i
+#         for j in range(i + 1, len(arr)):
+#             if arr[j] < arr[idx]:
+#                 idx = j
                 
-        arr[i], arr[idx] = arr[idx], arr[i]
+#         arr[i], arr[idx] = arr[idx], arr[i]
 
-arr = [2, 6, 5, 1, 4]
+# arr = [2, 6, 5, 1, 4]
 
+# selection_sort(arr)
+# print(arr)
+
+def selection_sort(arr):
+    # Traverse through all array elements
+    for i in range(len(arr)):
+        # Find the minimum element in remaining unsorted array
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+                
+        # Swap the found minimum element with the first element of the 'unsorted' part of the array
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+# Test the function
+arr = [64, 25, 12, 22, 11]
 selection_sort(arr)
-print(arr)
+print ("Sorted array is:", arr)
