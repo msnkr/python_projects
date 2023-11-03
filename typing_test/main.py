2f76dadd428ae6da9bc3e712a62db643ff9ef61a
msnkr <mikyle.designs@gmail.com>
2023-11-03 23:22:17 +0200

    .

--- typing_test/main.py
+++ typing_test/main.py
@@ -1,4 +1,12 @@
 import keyboard
+import faker
 
 # Display a list of random words
-# 
+# Get user input
+# compare the two
+# Find a way to calculate words per minute
+
+fake = faker.Faker()
+word_lst = " ".join([fake.word().lower() for _ in range(20)])
+
+print(word_lst)
