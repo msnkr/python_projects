36204d6055049ded43ef16825dd88434baeb2e81
msnkr <mikyle.designs@gmail.com>
2023-11-03 11:18:03 +0200

    .

--- weather_api_project/main.py
+++ weather_api_project/main.py
@@ -1,29 +1,43 @@
 import math
-from tkinter import Tk
+from tkinter import *
 import requests
+
+
+key = "888fc6acf8d5a7a7b9e51209de4178cc"
+req = requests.get(
+    "https://api.openweathermap.org/data/2.5/weather?q=Johannesburg,ZA&appid={}".format(key))
+
+if req.ok:
+    response = req.json()
+
+    temp, weather_description = math.floor(
+        response["main"]["temp"] / 10), response["weather"][0]["main"]
+
+    icon_bitmap = response['weather'][0]['icon']
+
+    icon = f"https://openweathermap.org/img/wn/{icon_bitmap}.png"
 
 
 window = Tk()
 
+
 window.title("Weather API")
 window.geometry("600x400")
 
+window.iconbitmap(icon)
+
+country_label = Label(window, text="Weather for Johannesburg")
+country_label.pack()
+country_label.config(font=("Arial", 22, "bold"))
 
 
+weather_label = Label(window, text="{}C".format(temp))
+weather_label.pack()
+
+description_label = Label(window, text=weather_description)
+description_label.pack()
+
+canvas = Canvas(window, width=300, height=200, background="white")
+canvas.pack()
 
 window.mainloop()
-
-# key = "888fc6acf8d5a7a7b9e51209de4178cc"
-
-# url = f"https://api.openweathermap.org/data/2.5/weather?q=Johannesburg,ZA&appid={key}"
-
-# req = requests.get(url)
-
-# if req.ok:
-#     response = req.json()
-
-#     temp = math.floor(response["main"]["temp"] / 10)
-#     weather_description = response["weather"][0]["main"]
-
-
-# print("{}. The temp is {}°C ".format(weather_description, temp))
