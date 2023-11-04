8b9906bf7b088e8f408843f8ce1a3849f721e1cd
msnkr <mikyle.designs@gmail.com>
2023-11-04 23:25:44 +0200

    .

--- typing_test/main.py
+++ typing_test/main.py
@@ -5,6 +5,18 @@ import faker
 fake = faker.Faker()
 
 
+def countdown_timer(count):
+    timer.config(text=count)
+
+
+def start():
+    canvas_text.insert(END, word_list())
+
+    count = 30
+    timer.after(1000, countdown_timer, count)
+    count -= 1
+
+
 def word_list():
     return " ".join([fake.word().lower() for _ in range(30)])
 
@@ -13,22 +25,21 @@ window = Tk()
 
 
 window.title("Eggghh")
-window.config(bg="grey", padx=20, pady=20)
+window.config(bg="darkgrey", padx=20, pady=20)
 
 
 timer = Label(window, text="00")
 timer.grid(row=0, column=1)
-timer.config(bg="grey", font=("Arial", 22, "bold"))
+timer.config(bg="darkgrey", font=("Arial", 22, "bold"))
 
 canvas_words = Canvas(window, width=300, height=200)
 canvas_words.grid(row=1, column=0)
 
 canvas_text = Text(canvas_words, width=40, height=10, padx=20, pady=20)
 canvas_text.grid(row=1, column=0)
-canvas_text.insert(END, word_list())
 
 
-start_btn = Button(window, text="Start")
+start_btn = Button(window, text="Start", command=start)
 start_btn.grid(row=1, column=1)
 start_btn.config(padx=5)
 
