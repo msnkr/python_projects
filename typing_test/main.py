e2f8833eafc27f6e7ce464c183305b20e11b91c4
msnkr <mikyle.designs@gmail.com>
2023-11-04 19:44:24 +0200

    .

--- typing_test/main.py
+++ typing_test/main.py
@@ -4,11 +4,37 @@ import faker
 
 fake = faker.Faker()
 
+
 def word_list():
-    return " ".join([fake.word().lower() for _ in range(20)])
+    return " ".join([fake.word().lower() for _ in range(30)])
 
 
+window = Tk()
 
+window.title("Eggghh")
+window.config(bg="grey", padx=20, pady=20)
+
+canvas_words = Canvas(window, width=300, height=200)
+canvas_words.grid(row=0, column=0)
+
+canvas_text = Text(canvas_words, width=40, height=10, padx=20, pady=20)
+canvas_text.grid(row=0, column=0)
+canvas_text.insert(END, word_list())
+
+
+start_btn = Button(window, text="Start")
+start_btn.grid(row=0, column=1)
+start_btn.config(padx=5)
+
+
+canvas_type = Canvas(window, width=300, height=200)
+canvas_type.grid(row=0, column=2)
+
+canvas_type = Text(canvas_type, width=40, height=10, padx=20, pady=20)
+canvas_type.grid(row=0, column=0)
+canvas_type.insert(END, "")
+
+window.mainloop()
 
 
 # Create app using tkinter
