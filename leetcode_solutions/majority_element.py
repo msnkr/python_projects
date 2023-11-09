381e475a47c99a3412d8a7f82e1518a8b1105490
msnkr <mikyle.designs@gmail.com>
2023-11-09 20:57:06 +0200

    .

--- leetcode_solutions/majority_element.py
+++ leetcode_solutions/majority_element.py
@@ -28,23 +28,22 @@
 # print("The majority element is:", result)
 
 
-def majorityElement(arr):
-    count = 1
-    majority_element = arr[0]
+# def majorityElement(arr):
+#     count = 1
+#     majority_element = arr[0]
 
-    for i in range(1, len(arr)):
+#     for x in range(1, len(arr)):
+#         if count == 0:
+#             majority_element = arr[x]
+#             count = 1
+#         elif majority_element == arr[x]:
+#             count + 1
+#         else:
+#             count -= 1
 
-        if count == 0:
-            majority_element = arr[i]
-            count = 1
-        elif majority_element == arr[i]:
-            count += 1
-        else:
-            count -= 1
-
-    return majority_element
+#     return majority_element
 
 
-nums = [3, 1, 1, 3, 4, 1, 1, 2, 4, 4, 2, 4, 4, 1, 1, 1]
-result = majorityElement(nums)
-print("The majority element is:", result)
+# nums = [3, 1, 1, 3, 4, 1, 1, 2, 4]
+# result = majorityElement(nums)
+# print("The majority element is:", result)
---
+++ leetcode_solutions/merged_lists.py
@@ -0,0 +1,49 @@
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Return the head of the merged linked list.
+
+
+# # Function to merge two sorted lists
+# def merged_lists(l1, l2):
+#     # Initialize a new list
+#     merged_list = []
+
+#     # While both lists have elements
+#     while l1 and l2:
+#         # If the first element of list1 is smaller
+#         if l1[0] < l2[0]:
+#             # Add it to the merged list
+#             merged_list.append(l1.pop(0))
+#         else:
+#             # Otherwise, add the first element of list2
+#             merged_list.append(l2.pop(0))
+
+#     # If there are remaining elements in either list, add them to the merged list
+#     merged_list.extend(l1 if l1 else l2)
+
+#     return merged_list
+
+
+# list1, list2 = [1, 2, 4], [1, 3, 4]
+# results = merged_lists(list1, list2)
+# print(results)
+
+# def merged_lists(arr1, arr2):
+#     merged_list = []
+
+#     while arr1 and arr2:
+#         if arr1[0] < arr2[0]:
+#             merged_list.append(arr1.pop(0))
+#         else:
+#             merged_list.append(arr2.pop(0))
+
+#     merged_list.extend(arr1 if arr1 else arr2)
+
+#     return merged_list
+
+
+# list1, list2 = [1, 2, 4], [1, 3, 4]
+# results = merged_lists(list1, list2)
+# print(results)
