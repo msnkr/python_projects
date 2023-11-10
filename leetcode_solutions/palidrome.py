a77e9d806cb40405ee0933bbbf59f4b3002a553d
msnkr <mikyle.designs@gmail.com>
2023-11-10 13:56:04 +0200

    .

--- leetcode_solutions/palidrome.py
+++ leetcode_solutions/palidrome.py
@@ -45,6 +45,20 @@
 # print(palindrome(23232323231))
 
 
+def palindrome_checker(x):
+    str_x = str(x)
+
+    left, right = 0, len(str_x) - 1
+
+    while left < right:
+        if str_x[left] != str_x[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
 
 
-results = 
+results = palindrome_checker(1551)
+print(results)
