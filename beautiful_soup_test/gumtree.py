c2b815a3060bc7fad3636dbdd2c8a52835a1fd6c
msnkr <mikyle.designs@gmail.com>
2023-11-13 22:38:07 +0200

    .

--- beautiful_soup_test/gumtree.py
+++ beautiful_soup_test/gumtree.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+from termcolor import colored
 
 
 url = requests.get("https://www.gumtree.co.za/s-all-the-ads/v1b0p1?q=python")
 if url.ok:
     soup = BeautifulSoup(url.content, "html.parser")
-    job_title = soup.find_all("div", class_="title")
     job_url = soup.find_all("a", class_="related-ad-title")
 
     for url in job_url:
-        print(url)
+        job_title = url.text
+        job_href = soup.find("a", class_="related-ad-title").get("href")
+
+        new_url = requests.get("https://www.gumtree.co.za{}".format(job_href))
+        job_description = BeautifulSoup(new_url.content, "html.parser")
+
+        job_description_text = job_description.find_all(
+            "div", class_="description")
+
+        for description in job_description_text:
+            job_description_text_1 = soup.find("span")
+
+            job_description_text_2 = soup.find("p")
+
+            print(job_description_text_2.text)
