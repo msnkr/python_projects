2a3a008e9e9cb489ba3a848e5373375c7d900e8c
msnkr <mikyle.designs@gmail.com>
2023-11-27 12:28:59 +0200

    .

--- word_meaning/main.py
+++ word_meaning/main.py
@@ -1,9 +1,22 @@
 from PyDictionary import PyDictionary
+from termcolor import colored
 
 dictionary = PyDictionary()
-word = input("What word do you want to check?: ")
 
-dict_word = dictionary.meaning(word)
 
-for x in dict_word:
-    print(x, dict_word[x])
+flag = True
+while flag:
+    word = input("What word do you want to check?: ")
+
+    if word != 'q':
+        dict_word = dictionary.meaning(word)
+
+        for x in dict_word:
+            print(colored(x, "green"))
+            print(colored(" ".join(dict_word[x]), "blue"))
+
+        print("=" * 200)
+
+    else:
+        print("Goodbye!")
+        flag = False
