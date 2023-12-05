5c3226318dbb3493519042f68eb658efff2a72f6
msnkr <mikyle.designs@gmail.com>
2023-12-05 16:03:22 +0200

    .

--- imageAI/main.py
+++ imageAI/main.py
@@ -4,12 +4,12 @@ import os
 execution_path = os.getcwd()
 
 prediction = ImageClassification()
-prediction.setModelTypeAsInceptionV3
+prediction.setModelTypeAsInceptionV3()
 prediction.setModelPath(os.path.join(
-    execution_path, "inception_v3_google-1a9a5a14.pth"))
-prediction.loadModel()
+    execution_path, "imageAI\models\inception_v3_google-1a9a5a14.pth"))
 
+prediction.loadModel()
 predictions, probabilities = prediction.classifyImage(
-    os.path.join(execution_path, "\imageAI\images\deer.jpg"), result_count=5)
+    os.path.join(execution_path, "imageAI\images\\rhino.jpeg"), result_count=5)
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction, " : ", eachProbability)
