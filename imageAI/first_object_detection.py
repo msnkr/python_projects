f413d2537ca30675a108c7ff86a2a16c9de0f069
msnkr <mikyle.designs@gmail.com>
2023-12-06 11:17:32 +0200

    .

--- imageAI/first_object_detection.py
+++ imageAI/first_object_detection.py
@@ -9,7 +9,7 @@ detector.setModelPath(os.path.join(
     execution_path, "imageAI/tiny-yolov3.pt"))
 detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image=os.path.join(
-    execution_path, "imageAI/images/unsplash-2.jpg"), output_image_path=os.path.join(execution_path, "imageAI/images/image2new.jpg"), minimum_percentage_probability=90)
+    execution_path, "imageAI/images/unsplash-3.jpg"), output_image_path=os.path.join(execution_path, "imageAI/images/image2new.jpg"), minimum_percentage_probability=10)
 
 for eachObject in detections:
     print(eachObject["name"], " : ", eachObject["percentage_probability"],
