8e0e813964af72e1ffa3e90c941914abfb384830
msnkr <mikyle.designs@gmail.com>
2025-04-14 15:20:00 +0200

    .

---
+++ Streamlit Test/app.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+
+tickers = ["ZAR=X", "EUR=X", "USD=X", "GBP=X"]
+
+data={}
+for ticker in tickers:
+    data[ticker] = yf.Ticker(ticker).history(period="40y")["Close"]
+
+
+st.write("""
+
+# Simple Stock Price App
+         South Africa vs European Close Price
+         """)
+
+
+combined_data = pd.DataFrame(data)
+st.line_chart(combined_data)
