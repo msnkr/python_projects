9826a5dadae5d7ba5862e67f9d842f098a8072fe
msnkr <mikyle.designs@gmail.com>
2025-04-14 16:13:56 +0200

    .

--- Streamlit Test/app.py
+++ Streamlit Test/app.py
@@ -1,21 +1,27 @@
-import yfinance as yf
+# import yfinance as yf
+# import streamlit as st
+# import pandas as pd
+
+
+# tickers = ["ZAR=X", "EUR=X", "USD=X", "GBP=X"]
+
+# data={}
+# for ticker in tickers:
+#     data[ticker] = yf.Ticker(ticker).history(period="40y")["Close"]
+
+
+# st.write("""
+
+# # Simple Stock Price App
+#          South Africa vs European Close Price
+#          """)
+
+
+# combined_data = pd.DataFrame(data)
+# st.line_chart(combined_data)
+
+
 import streamlit as st
 import pandas as pd
+from sklearn.datasets import load_iris
 
-
-tickers = ["ZAR=X", "EUR=X", "USD=X", "GBP=X"]
-
-data={}
-for ticker in tickers:
-    data[ticker] = yf.Ticker(ticker).history(period="40y")["Close"]
-
-
-st.write("""
-
-# Simple Stock Price App
-         South Africa vs European Close Price
-         """)
-
-
-combined_data = pd.DataFrame(data)
-st.line_chart(combined_data)
